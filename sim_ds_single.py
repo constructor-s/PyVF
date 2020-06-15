b68b213bdd7778fb20a4466cb4ae53d49cdb5d99
Bill <shirunjie@126.com>
2020-06-15 11:10:42 -0400

    Add Turpin 2003 like plot

--- sim_ds_single.py
+++ sim_ds_single.py
@@ -63,17 +63,83 @@ def simPerfectSingleStaircase(true_threshold, starting_threshold):
         stimulus, threshold = strategy.get_stimulus_threshold(data)
 
     _logger.info("%3d: %s\t%s", counter, threshold, stimulus)
-    return data
+    return data, threshold
+
+
+def sim_ds_single_offsets():
+    true_thresholds = np.array([-0.001, 25, 33, 40.001])
+    starting_threshold_offsets = np.arange(-10.5, 11.5, 1.0)
+    data_collection = []
+    for i, true_threshold in enumerate(true_thresholds):
+        data_collection.append([])
+        for j, offset in enumerate(starting_threshold_offsets):
+            data = simPerfectSingleStaircase(true_threshold=true_threshold, starting_threshold=true_threshold + offset)
+            data_collection[i].append(data)
+    # Calculate how many presentations did it take for each test condition
+    presentations = [[len(x[0]) for x in l] for l in data_collection]
+    presentations = np.array(presentations)
+    final_estimate = [[x[1][0] for x in l] for l in data_collection]
+    final_estimate = np.array(final_estimate)
+    # Plotting
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots(len(true_thresholds), len(starting_threshold_offsets),
+                           sharex='col', sharey='row', figsize=(10.5, 4.5))
+    for i in range(ax.shape[0]):
+        _logger.info("%d", i)
+        for j in range(ax.shape[1]):
+            data = data_collection[i][j][0]
+            data = Stimulus.to_numpy(data)
+            ax[i, j].plot(data[TSDISP], data[THRESHOLD], 'k-',
+                          data[TSDISP][data[RESPONSE] == STIMULUS_SEEN],
+                          data[THRESHOLD][data[RESPONSE] == STIMULUS_SEEN], 'go',
+                          data[TSDISP][data[RESPONSE] == STIMULUS_NOT_SEEN],
+                          data[THRESHOLD][data[RESPONSE] == STIMULUS_NOT_SEEN], 'rx',
+                          [0, len(data[TSDISP]) - 0.5], [true_thresholds[i], true_thresholds[i]], 'k:'
+                          )
+            ax[i, j].set_facecolor(plt.get_cmap('Reds', 10)(len(data) - 3))
+    fig.savefig("sim_ds_single.png")
+
+
+def sim_ds_single_turpin_2003_fig5():
+    true_thresholds = np.arange(0, 40.1, 1)
+    starting_thresholds = np.array([10, 20, 30])
+    data_collection = []
+    for i, true_threshold in enumerate(true_thresholds):
+        data_collection.append([])
+        for j, starting_threshold in enumerate(starting_thresholds):
+            data = simPerfectSingleStaircase(true_threshold=true_threshold, starting_threshold=starting_threshold)
+            data_collection[i].append(data)
+    # Calculate how many presentations did it take for each test condition
+    presentations = [[len(x[0]) for x in l] for l in data_collection]
+    presentations = np.array(presentations)
+    final_estimate = [[x[1][0] for x in l] for l in data_collection]
+    final_estimate = np.array(final_estimate)
+
+    # Plotting
+    import matplotlib.pyplot as plt
+
+    for j, starting_threshold in enumerate(starting_thresholds):
+        fig, ax = plt.subplots(2, 1, sharex='col', sharey='row', figsize=(8.5, 11))
+        ax[0].plot(true_thresholds, presentations[:, j], 'k.-')
+        ax[0].set_ylabel("number of presentations")
+        ax[0].set_yticks(np.arange(0, 16.1, 2))
+        ax[0].grid(True)
+        ax[0].legend(["4-2 staircase, no retest"])
+
+        ax[1].plot(true_thresholds, final_estimate[:, j] - true_thresholds, 'k.-')
+        ax[1].set_ylabel("error (dB)")
+        ax[1].set_yticks(np.arange(-20, 20.1, 5))
+        ax[1].set_xlabel("Input threshold (dB)")
+        ax[1].set_xticks(np.arange(0, 40.1, 5))
+        ax[1].grid(True)
+        ax[1].set_aspect('equal', adjustable='datalim')
+
+        fig.suptitle(f"Starting estimate = {starting_threshold}dB, No variability")
+        fig.savefig(f"sim_ds_single_{starting_threshold}.pdf")
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    data_collection = []
-    for i, true_threshold in enumerate(range(40)):
-        data_collection.append([])
-        for j, starting_threshold in enumerate(range(40)):
-            data = simPerfectSingleStaircase(true_threshold=true_threshold, starting_threshold=starting_threshold)
-            data_collection[i].append(data)
 
-    presentations = [[len(x) for x in l] for l in data_collection]
-    presentations = np.array(presentations)
+    # sim_ds_single_offsets()
+    sim_ds_single_turpin_2003_fig5()
