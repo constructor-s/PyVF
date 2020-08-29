542774d291a1d958cd37428fa9d6f6037c8debe0
Bill <shirunjie@126.com>
2020-08-28 23:49:10 -0400

    WIP: HFA PDF parse VF, MD, PD

--- pdf_parser.py
+++ pdf_parser.py
@@ -10,44 +10,109 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
 
-class TextCollectorDevice(PDFLayoutAnalyzer):
-    def receive_layout(self, ltpage):
-        self.ltpage = ltpage
-        self.items = []
-        self.lines = []
-        self.curr_x = None
-        self.curr_y = None
-        self.render(ltpage)
+class HFASFADevice(PDFLayoutAnalyzer):
+    def __init__(self, rsrcmgr, pageno=1, laparams=None):
+        super(HFASFADevice, self).__init__(rsrcmgr, pageno=1, laparams=None)
+        self.text_sequences = []
 
-    def render(self, item):
-        if isinstance(item, LTContainer):
-            for child in item:
-                self.render(child)
-        elif isinstance(item, LTText):
-            self.items.append(item)
-            a, b, c, d, x, y = item.matrix
-            if x == self.curr_x or y == self.curr_y:
-                self.lines[-1].append(item.get_text())
-            else:
-                self.lines.append([item.get_text()])
-            self.curr_x = x
-            self.curr_y = y
-
-        if isinstance(item, LTTextBox):
-            return
-        elif isinstance(item, LTImage):
-            return
-        else:
-            return
+    def render_string(self, textstate, seq, ncs, graphicstate):
+        super(HFASFADevice, self).render_string(textstate, seq, ncs, graphicstate)
+        font = textstate.font
+        for obj in seq:
+            self.text_sequences.append("".join([font.to_unichr(c) for c in font.decode(obj)]))
 
 
-with open(sys.argv[1], 'rb') as in_file:
-    parser = PDFParser(in_file)
-    doc = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    device = TextCollectorDevice(rsrcmgr)  # TextConverter(rsrcmgr, output_string, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.create_pages(doc):
-        interpreter.process_page(page)
+class HFASFATextParser:
+    def __init__(self, text_sequences):
+        self.text_sequences = text_sequences
 
-    print('\n'.join(''.join(i) for i in device.lines))
+    def get_value(self, key, offset_start=1):
+        key_index = self.text_sequences.index(key)
+        value = self.text_sequences[key_index + offset_start]
+        return value
+
+    def get_value_list(self, key, offset_start=1, length=1):
+        key_index = self.text_sequences.index(key)
+        value = self.text_sequences[key_index + offset_start:key_index + offset_start + length]
+        return value
+
+    @property
+    def name(self):
+        return self.get_value("Patient:")
+
+    @property
+    def dob(self):
+        return self.get_value("Date of Birth:")
+
+    @property
+    def gender(self):
+        return self.get_value("Gender:")
+
+    @property
+    def id(self):
+        return self.get_value("Patient ID:")
+
+    @property
+    def laterality(self):
+        value = self.get_value("Patient ID:", offset_start=2)
+        assert value == "OS" or value == "OD"
+        return value
+
+    @property
+    def report_type(self):
+        value = self.get_value("Patient ID:", offset_start=3)
+        assert value == "Single Field Analysis"
+        return value
+
+    @property
+    def pattern(self):
+        value = self.get_value("Patient ID:", offset_start=4)
+        return value
+
+    @property
+    def n_vf_loc(self):
+        if self.pattern == "Central 24-2 Threshold Test":
+            return 54
+
+    @property
+    def n_td_loc(self):
+        if self.pattern == "Central 24-2 Threshold Test":
+            return 52
+
+    @property
+    def vf(self):
+        value_list = self.get_value_list("Age:", offset_start=7, length=self.n_vf_loc)
+        for i in value_list:
+            assert i == "<0" or float(i) is not None
+        return value_list
+
+    @property
+    def td(self):
+        value_list = self.get_value_list("Age:", offset_start=7+self.n_vf_loc, length=self.n_td_loc)
+        for i in value_list:
+            assert float(i) is not None
+        return value_list
+
+    @property
+    def pd(self):
+        value_list = self.get_value_list("Age:", offset_start=7 + self.n_vf_loc+self.n_td_loc, length=self.n_td_loc)
+        for i in value_list:
+            assert float(i) is not None
+        return value_list
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = HFASFADevice(rsrcmgr)  # TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+
+        sfa = HFASFATextParser(device.text_sequences)
+        print(sfa.name, sfa.dob, sfa.gender, sfa.id, sfa.laterality)
+        print(sfa.vf)
+        print(sfa.td)
+        print(sfa.pd)
