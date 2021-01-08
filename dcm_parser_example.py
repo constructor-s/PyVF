c10090bedf392f75ec88cfea320e943c5fb2656c
B <10003477+constructor-s@users.noreply.github.com>
2021-01-08 15:38:41 -0500

    Make parser example accept glob and multiple inputs

--- dcm_parser_example.py
+++ dcm_parser_example.py
@@ -1,4 +1,4 @@
-from pyvf.parse.pdf import HFAPDFParser
+from pyvf.parse.dcm import HFADCMParser
 import argparse
 from itertools import chain
 from glob import glob
@@ -6,36 +6,33 @@ import logging
 _logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Demo of HFA PDF SFA parsing")
-    parser.add_argument("-i", "--input", required=True, nargs="+", help="input.pdf")
+    parser = argparse.ArgumentParser("Demo of HFA DCM SFA parsing")
+    parser.add_argument("-i", "--input", required=True, nargs="+", help="input.dcm")
     parser.add_argument("-v", "--verbose", action="store_true")
     args = parser.parse_args()
 
     logging.basicConfig()
     _logger.setLevel(logging.DEBUG if args.verbose else logging.INFO)
-    logging.getLogger('pyvf.parse.pdf').setLevel(logging.DEBUG if args.verbose else logging.INFO)
+    logging.getLogger('pyvf.parse.dcm').setLevel(logging.DEBUG if args.verbose else logging.INFO)
 
     for inp in chain(*map(glob, args.input)):
         _logger.info("Input: %s", inp)
         with open(inp, "rb") as f:
-            parser = HFAPDFParser(f)
-        try:
-            print(parser.id)
-        except ValueError as e:
-            _logger.error("Cannot parse ID: %s", e)
+            parser = HFADCMParser(f)
+        print(parser.id)
         print(parser.laterality)
         print(parser.report_type)
         print(parser.pattern)
-        print(parser.vf)
-        print(parser.td)
-        print(parser.pd)
+        print(parser.pdf_parser.vf)
+        print(parser.pdf_parser.td)
+        print(parser.pdf_parser.pd)
         print(parser.false_positive)
         print(parser.false_negative)
         print(f"{parser.fixation_loss_error}/{parser.fixation_loss_total}")
-        print(parser.test_duration)
-        print(parser.date)
-        print(parser.time)
-        print(parser.age)
+        print(parser.pdf_parser.test_duration)
+        print(parser.datetime)
+        # print(parser.time)
+        print(parser.dob)
         print(parser.md)
         print(parser.psd)
         print(parser.vfi)
